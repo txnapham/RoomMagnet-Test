07467583b6a54cc6958035b1b311a94667db402e
ramakrat <43589549+ramakrat@users.noreply.github.com>
2019-11-04 22:28:57 -0500

    testing search functionality

--- RoomMagnet Test/Search.aspx.cs
+++ RoomMagnet Test/Search.aspx.cs
@@ -15,8 +15,8 @@ using System.Data.SqlClient;
 
 public partial class Search : System.Web.UI.Page
 {
-
     System.Data.SqlClient.SqlConnection sqlConn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+    System.Data.SqlClient.SqlConnection sqlConn2 = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -43,26 +43,38 @@ public partial class Search : System.Web.UI.Page
             //select.CommandText = "select [City], [HomeState], [Zip], [RoomPriceRangeLow], [RoomPriceRangeHigh] from [dbo].[Property] " + "Where [City] = @City AND [HomeState] = @HomeState";
             //select.Parameters.AddWithValue("@City", city);
             //select.Parameters.AddWithValue("@State", state);
+            Card1.Text = "";
 
             sqlConn.Open();
+            sqlConn2.Open();
             String classType = "card";
             String imgSource = "";
             String cardBody = "";
+
             String tSearch = HttpUtility.HtmlEncode(txtSearch.Text);
             int commaSplit = tSearch.IndexOf(",");
             String cityString = tSearch.Substring(0, commaSplit).ToUpper();
             String state = tSearch.Substring(commaSplit + 2).ToUpper(); 
-            String query = "select [City], [HomeState], [Zip], [RoomPriceRangeLow],[RoomPriceRangeHigh] from[dbo].[Property] where upper([City]) like '" + cityString + "' AND upper([HomeState]) like '"+state+ "';";
+
+            String query = "select [City], [HomeState], [Zip], [RoomPriceRangeLow], [RoomPriceRangeHigh] from [dbo].[Property] where upper([City]) like '" + cityString + "' AND upper([HomeState]) like '"+state+ "';";
+            String countResults = "select count(*) from [dbo].[Property] where upper([City]) like '" + cityString + "' AND upper([HomeState]) like '" + state + "';";
+
             System.Data.SqlClient.SqlCommand sqlComm = new System.Data.SqlClient.SqlCommand(query, sqlConn);
+            System.Data.SqlClient.SqlCommand sqlCount = new System.Data.SqlClient.SqlCommand(countResults, sqlConn2);
             //sqlComm.CommandText = ("select [City], [HomeState], [Zip], [RoomPriceRangeLow], [RoomPriceRangeHigh] from[dbo].[Property] where upper([City]) like upper('%" + cityString + "%')"); 
             System.Data.SqlClient.SqlDataReader reader = sqlComm.ExecuteReader();
+            int resultCount = (int)sqlCount.ExecuteScalar();
+            int runCount = 0;
+
             while (reader.Read())
             {
                 String city = reader["City"].ToString();
                 String homeState = reader["HomeState"].ToString();
                 String priceRangeLow = reader["RoomPriceRangeLow"].ToString();
                 String priceRangeHigh = reader["RoomPriceRangeHigh"].ToString();
-
+                priceRangeLow = String.Format("{0:0.00}", priceRangeLow);
+                priceRangeHigh = String.Format("{0:0.00}", priceRangeHigh);
+                
                 StringBuilder myCard = new StringBuilder();
                 myCard
                     .Append("<div class=\"col-md-3\">")
@@ -79,11 +91,13 @@ public partial class Search : System.Web.UI.Page
                     .Append("                            <button id=\"heartbtn\" class=\"btn favoriteHeartButton\"><i id=\"hearti\" class=\"far fa-heart\"></i></button>")
                     .Append("                        </div>")
                     .Append("                    </div>")
+                    .Append("           </div>")
                     .Append("</div>");
 
-	
 
+                runCount++;
                 Card1.Text += myCard.ToString();
+
             }
             reader.Close();
 
@@ -92,12 +106,6 @@ public partial class Search : System.Web.UI.Page
             //    + "<a href=" + "search-result-page-detail.html class=" + "cardLinks" + ">"
             //    + "<div class=" + cardBody + "\">"  + "<h5 class=" + cardTitle +
 
-
-
-
-
         }
-
     }
-
 }
