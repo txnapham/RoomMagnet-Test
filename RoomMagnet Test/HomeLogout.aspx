dabe28f16080a4b5776eb9971ce514f4fc1d1016
ramakrat <43589549+ramakrat@users.noreply.github.com>
2019-11-11 19:36:15 -0500

    logout function and aws packages

--- RoomMagnet Test/HomeLogout.aspx
+++ RoomMagnet Test/HomeLogout.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="HomeLogout.aspx.cs" Inherits="HomeLogout" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
--- RoomMagnet Test/HomeLogout.aspx.cs
+++ RoomMagnet Test/HomeLogout.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,11 +12,23 @@ public partial class HomeLogout : System.Web.UI.Page
     protected void Page_PreInit(object sender, EventArgs e)
     {
         Session.Clear();
-        Response.Redirect("Home.aspx");
+        if (Session["type"] != null)
+        {
+            this.MasterPageFile = "~/MasterPage.master";
+        }
+        else if (Session["type"] == null)
+        {
+            this.MasterPageFile = "~/MasterPage.master";
+        }
     }
 
 
     protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Redirect("Home.aspx");
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
     {
 
     }
